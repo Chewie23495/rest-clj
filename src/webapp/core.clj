712168009d30d21b0de3bea703b98803a021a13c
Chewie23495 <61468183+Chewie23495@users.noreply.github.com>
2020-12-30 11:27:17 +0100

    Update core.clj

--- src/webapp/core.clj
+++ src/webapp/core.clj
@@ -8,13 +8,6 @@
             [ring.middleware.cors :refer [wrap-cors]])
   (:gen-class))
 
-#_(def request-schema
-     {:address
-           {:colorKeys [s/Str]
-            :values [s/Num]}
-      :meta {:digits s/Num
-             :processingPattern s/Str}})
-
 
 (defn body-json-handler [request]
   (ring-json/json-body-request request {:keywords? true :bigdecimals? false}))
@@ -44,31 +37,3 @@
 
 (defn -main [port]
   (jetty/run-jetty app {:port (Integer. port)}))
-
-
-
-"(defn handler [request]
-  (ring.util.response/response
-    (clojure.data.json/read-str request :key-fn keyword)))
-
-(defn body-json-handler [request]
-  (println
-    (ring-json/json-body-request request {:keys [true true]})))
-
-(defn get-values [request]
-  (:values(:address(:body request))))
-
-(defn sum-values [vector]
-  (reduce + vector))
-
-(defn sum-of-digits [n]
-  (loop [count n acc 0]
-    (if (zero? count) acc
-                  (recur
-                              (quot count 10)
-                              (+ acc (mod count 10))))))
-
-(defn handler [request]
-  (response
-    (get-values
-      (body-json-handler request))))"
